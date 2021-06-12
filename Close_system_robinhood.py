6cdb18bd058912e42533bb5bc7652a83996e16a0
Nit <83890233+nitiphong-nira@users.noreply.github.com>
2021-06-12 15:08:33 -0500

    Update Close_system_robinhood.py

--- Close_system_robinhood.py
+++ Close_system_robinhood.py
@@ -24,19 +24,24 @@ ask = float('%.2f' % float(r.get_crypto_quote("BTC")['ask_price']))
 ask = int(float('%.2f' % (ask/10000))* 10000)-100
 bid = float('%.2f' % float(r.get_crypto_quote("BTC")['bid_price']))
 bid = int(float('%.2f' % (bid/10000))* 10000)
-
+BTC = pd.DataFrame(data =r.get_crypto_positions(info=None))
+Btc_avilable = float(max(BTC['quantity_available'].tolist()))
+    
 #------------------------------------------------------------------------
-
-def check(Start_buy,Start_sell):
-    Open = pd.DataFrame(data = Open_order[['side']])
-    BUY = min(min(Open.values.tolist()))
-    SELL = max(max(Open.values.tolist()))
+def parameter():
     low = Open_order[['price','side']].groupby(['side']).min()
     lower_zone = int('%.d' % (float(min(low.values.tolist()[0]))))
     high = Open_order[['price','side']].groupby(['side']).max()
     print("Your BTC lowest zone is", lower_zone)
     min_sell = int('%.d' % (float(min(low.values.tolist()[1]))))
+    return (low,lower,high,min_sell)
+
+def check(Start_buy):
+    Open = pd.DataFrame(data = Open_order[['side']])
+    BUY = min(min(Open.values.tolist()))
+    SELL = max(max(Open.values.tolist()))
     if BUY == "buy":
+        low,lower,high,min_sell = parameter()
         max_buy = int('%.d' % (float(max(high.values.tolist()[0]))))
         buying = max_buy
         if SELL == "sell":
@@ -47,17 +52,22 @@ def check(Start_buy,Start_sell):
     else:
         buying = Start_buy
         if SELL == "sell":
-            selling = min_sell
+            if (Btc_avilable-0.00001)>= 0:
+                low = Open_order[['price','side']].groupby(['side']).min()
+                selling = int('%.d' % (float(min(low.values.tolist()[1]))))
+            else:
+                selling = 99999999
         else:
             selling = int(input("There are some BTC in the port. How much do you want to sell : "))
         return(buying,selling)
 
-def BUY(buying,frequency,Start_buy):
+def BUY(buying,frequency,Start_buy,lot_side):
     while (buying <= bid-200):
+        buying = buying + frequency
         if buying <= Start_buy:
             print("Your buy orders are matched")
+            time.sleep(1)
         else:
-            buying = buying + frequency
             r.order_buy_crypto_limit('BTC',lot_side,buying)
             print("Buy : ", buying)
             time.sleep(1)
@@ -67,29 +77,7 @@ def Open_order_report():
     print("\n-------------Summery Open Order Report-------------\n\n",Open_order[['price','side','state']].sort_values(by=['price']))   
     print("BTC avilible : ",Btc_avilable)
     
-def Sell(selling,frequency):
-    BTC = pd.DataFrame(data =r.get_crypto_positions(info=None))
-    Btc_avilable = float(max(BTC['quantity_available'].tolist()))
+def Sell(selling,frequency,Btc_avilable,lot_side):
     while((Btc_avilable-0.00001)>= 0):
         if (selling >= ask+100):
             selling = selling - frequency
-            r.order_sell_crypto_limit('BTC',lot_side,selling)
-            print("Sell : ", selling)
-        BTC = pd.DataFrame(data =r.get_crypto_positions(info=None))
-        Btc_avilable = float(max(BTC['quantity_available'].tolist()))
-    print("End of selling")
-        
-def loop(frequency):
-    Start_buy = int(input("Plase enter the BTC price that you want to buy as lowest zone: "))
-    frequency = int(input("Pleas enter frequency gap between the zone : "))
-    lot_side = float(input("Pleas enter lot side of the order : "))
-    for i in range (0,60):
-        buying,selling = check(Start_buy)
-        print(i+1 , "minutes")
-        BUY(buying,frequency,Start_buy)
-        Sell(selling,frequency)
-        Open_order_report()
-        time.sleep(60)
-        
-loop(frequency)
-
